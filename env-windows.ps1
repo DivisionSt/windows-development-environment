f0d27fc898cc2038aacefb2f192e95b041ac22d2
Humphrey <forrest.humphrey@edma010213-mbp.hps.com>
2019-07-24 11:07:57 -0500

    fixing powershellget issues

--- env-windows.ps1
+++ env-windows.ps1
@@ -24,9 +24,11 @@ function Push-User-Path($userPath) {
 }
 
 #
-# Stupid thing where you have to press enter (at the top for your convenience)
+# Stupid things where you might have to press enter (at the top o' script for your convenience)
 #
-Install-Module –Name PowerShellGet –Force
+Install-PackageProvider Nuget -Force
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+Install-Module -Name PowerShellGet -Force
 
 
 #
