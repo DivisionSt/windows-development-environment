46318eb1dd8bcc53113fd4e9d9e5183881667538
Humphrey <forrest.humphrey@edma010213-mbp.hps.com>
2019-07-24 10:03:01 -0500

    initial modifications for my preferred installs and environment

---
+++ assets/Microsoft.powershell_profile.ps1
@@ -0,0 +1,45 @@
+# Ensure that Get-ChildItemColor is loaded
+Import-Module Get-ChildItemColor
+
+# Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColor -Option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
+
+# Helper function to change directory to my development workspace
+# Change c:\ws to your usual workspace and everytime you type
+# in cws from PowerShell it will take you directly there.
+function go-code { Set-Location c:\code }
+
+# Helper function to set location to the User Profile directory
+function go-profile { Set-Location ~ }
+Set-Alias ~ go-profile -Option AllScope
+
+# Helper function to show Unicode character
+function U
+{
+    param
+    (
+        [int] $Code
+    )
+ 
+    if ((0 -le $Code) -and ($Code -le 0xFFFF))
+    {
+        return [char] $Code
+    }
+ 
+    if ((0x10000 -le $Code) -and ($Code -le 0x10FFFF))
+    {
+        return [char]::ConvertFromUtf32($Code)
+    }
+ 
+    throw "Invalid character code $Code"
+}
+
+# Ensure posh-git is loaded
+Import-Module -Name posh-git
+
+# Ensure oh-my-posh is loaded
+Import-Module -Name oh-my-posh
+
+# Default the prompt to agnoster oh-my-posh theme
+Set-Theme agnoster
--- env-windows.ps1
+++ env-windows.ps1
@@ -38,25 +38,19 @@ Get-Command -Module Microsoft.PowerShell.Archive
 #
 # Git
 #
-
 choco install git --yes --params '/GitAndUnixToolsOnPath'
-choco install tortoisegit --yes
 Update-Environment-Path
-git config --global alias.pom 'pull origin main'
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.st 'status'
+git config --global alias.br 'branch'
+git config --global alias.pom 'pull origin master'
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short"
 git config --global alias.ammend "commit -a --amend"
 git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
 git config --global alias.everything "! git pull && git submodule update --init --recursive"
 git config --global alias.aliases "config --get-regexp alias"
-
-# PowerShell Tooling for Git
-Install-Module posh-git -Force -Scope CurrentUser
-Install-Module oh-my-posh -Force -Scope CurrentUser
-Set-Prompt
-Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
-Add-Content $PROFILE "`nImport-Module posh-git`nImport-Module oh-my-posh`nSet-Theme Paradox"
-
 
 # Font to support PowerShell Tooling:
 choco install cascadiacode --yes
@@ -69,36 +63,33 @@ Write-Output 'Be sure to configure Windows Terminal fonts! Suggest using "fontFa
 #
 # AWS awscli
 #
-choco install awscli --yes
-Update-Environment-Path
+#choco install awscli --yes
+#Update-Environment-Path
 
 #
 # MinGW
 # 
-
 choco install mingw --yes
 Update-Environment-Path
-
 # Get-Command mingw32-make
-
 # todo: Alias `make` to `mingw32-make` in Git Bash
 # todo: Write `mingw32-make %*` to make.bat in MinGW install directory
 
 #
 # Caddy HTTP Server
 #
-
-choco install caddy --yes
-Update-Environment-Path
+#choco install caddy --yes
+#Update-Environment-Path
 
 #
 # Languages
 #
-choco install php --yes
-choco install ruby --yes
-choco install ruby2.devkit --yes
-choco install jdk8 --yes
-Update-Environment-Path
+#choco install php --yes
+#choco install ruby --yes
+#choco install ruby2.devkit --yes
+#choco install python2 --yes
+#choco install jdk8 --yes
+#Update-Environment-Path
 
 
 git clone https://github.com/pyenv-win/pyenv-win.git $env:USERPROFILE\.pyenv
@@ -122,27 +113,26 @@ npm install --global --production npm-windows-upgrade
 npm-windows-upgrade --npm-version latest
 npm install -g gulp-cli 
 npm install -g yo
-npm install -g mocha
+# npm install -g mocha
 npm install -g install-peerdeps
 npm install -g typescript
 
 #
 # Docker
 # 
-
 # Hyper-V required for docker and other things
-Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All -NoRestart
-
-choco install docker --yes
-choco install docker-machine --yes
-choco install docker-compose --yes
-choco install docker-for-windows --yes
-
-Update-Environment-Path
-
-docker pull worpress
-docker pull mysql
-docker pull phpmyadmin
+#Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All -NoRestart
+#
+#choco install docker --yes
+#choco install docker-machine --yes
+#choco install docker-compose --yes
+#choco install docker-for-windows --yes
+#
+#Update-Environment-Path
+#
+#docker pull worpress
+#docker pull mysql
+#docker pull phpmyadmin
 
 Update-Environment-Path
 
@@ -161,7 +151,7 @@ choco install kubernetes-cli --yes
 # ?? choco install yarn --yes
 
 # Bower
-npm install -g bower
+#npm install -g bower
 
 # Grunt
 npm install -g grunt-cli
@@ -169,7 +159,6 @@ npm install -g grunt-cli
 #
 # VS Code
 #
-
 choco install visualstudiocode --yes # includes dotnet
 Update-Environment-Path
 
@@ -182,19 +171,8 @@ Start-Process https://github.com/sdras/night-owl-vscode-theme
 
 # PowerShell support
 code --install-extension ms-vscode.PowerShell
-
 # CSharp support
 code --install-extension ms-vscode.csharp
-
-# PHP support
-code --install-extension HvyIndustries.crane
-
-# Ruby support
-code --install-extension rebornix.Ruby
-
-# C++ support
-code --install-extension ms-vscode.cpptools
-
 # HTML, CSS, JavaScript support
 code --install-extension Zignd.html-css-class-completion
 code --install-extension robinbentley.sass-indented
@@ -202,89 +180,71 @@ code --install-extension dbaeumer.vscode-eslint
 code --install-extension dzannotti.vscode-babel-coloring
 code --install-extension esbenp.prettier-vscode
 code --install-extension formulahendry.auto-rename-tag
-
 # NPM support
 code --install-extension eg2.vscode-npm-script
 code --install-extension christian-kohler.npm-intellisense
-
-# Jasmin Support
-code --install-extension hbenl.vscode-jasmine-test-adapter
-
-# Jest support
-code --install-extension Orta.vscode-jest
-
-# React Native support
-code --install-extension vsmobile.vscode-react-native
-npm install -g create-react-native-app
-npm install -g react-native-cli
-
+# # Mocha support
+# code --install-extension spoonscen.es6-mocha-snippets
+# code --install-extension maty.vscode-mocha-sidebar
+# # React Native support
+# code --install-extension vsmobile.vscode-react-native
+# npm install -g create-react-native-app
+# npm install -g react-native-cli
 # Docker support
-code --install-extension PeterJausovec.vscode-docker
-
-# PlantUML support
-code --install-extension jebbs.plantuml
-
+# code --install-extension PeterJausovec.vscode-docker
 # Markdown Support 
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension mdickin.markdown-shortcuts
 
-# WSL Support
-code --install-extension ms-vscode-remote.remote-wsl
-
-#
-# MySQL
-#
-
-choco install mysql --yes
-choco install mysql.workbench --yes
+# #
+# # MySQL
+# #
+# choco install mysql --yes
+# choco install mysql.workbench --yes
 
 
 #
 # Android Studio
 # 
-
-choco install androidstudio --yes
+# choco install androidstudio --yes
 
 #
 # Static Site Generators
 #
-
 # Hugo
-choco install hugo --yes
+# choco install hugo --yes
 
 #
 # Basic Utilities
 #
-
-# choco install slack --yes
+choco install slack --yes
+choco install notepadplusplus --yes
 # choco install xenulinksleuth --yes
 
 # InkScape
-choco install inkscape --yes
+#choco install inkscape --yes
 
 # Windows Terminal
 choco install microsoft-windows-terminal --yes
 
 # File Management
-choco install beyondcompare --yes
+# choco install beyondcompare --yes
 choco install 7zip --yes
-choco install filezilla --yes
-choco install dropbox --yes
-
-# Media Viewers
-choco install irfanview --yes
-choco install vlc --yes
+# choco install filezilla --yes
+# choco install dropbox --yes
 
 # Browsers
 choco install googlechrome --yes
+choco install googlechrome.canary --yes
 choco install firefox --yes
 
 # Misc
 choco install sysinternals --yes
 choco install procexp --yes
-choco install awscli --yes
+# choco install awscli --yes
 choco install firacode --yes # See https://www.youtube.com/watch?v=KI6m_B1f8jc
 choco install everything --yes
+choco install paint.net --yes
 
 Update-Environment-Path
 
@@ -294,5 +254,51 @@ Enable-WindowsOptionalFeature -Online -FeatureName $("VirtualMachinePlatform", "
 Update-Environment-Path
 wsl --set-default-version 2
 Start-Process https://aka.ms/wslstore
+
+#
+# Command-line niceness
+#
+Write-Output "installing nice powershell goodness. Buckle up."
+choco install conemu --yes
+
+if (!(Test-Path 'C:\tmp')) {
+    mkdir c:\tmp
+}
+if (!(Test-Path 'c:\tmp\fonts')) {
+    write-host "installing fonts"
+    git clone https://github.com/powerline/fonts.git c:\tmp\fonts
+    set-location c:\tmp\fonts
+    .\install.ps1
+}
+
+# PowerShell Tooling for Git
+Install-Module –Name PowerShellGet –Force
+Install-Module posh-git -Force -Scope CurrentUser
+Install-Module oh-my-posh -Force -Scope CurrentUser
+Install-Module -Name 'Get-ChildItemColor'
+Set-Prompt
+Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+Add-Content $PROFILE "`nImport-Module posh-git`nImport-Module oh-my-posh`nSet-Theme Paradox"
+
+
+# move in a nice powershell profile
+Set-ExecutionPolicy Unrestricted
+Unblock-File -Path "..\assets\Microsoft.PowerShell_profile.ps1"
+copy-item "..\assets\Microsoft.PowerShell_profile.ps1" "$env:USERPROFILE\Documents\WindowsPowerShell\"
+Set-ExecutionPolicy RemoteSigned
+
+# move in a nice conemu profile=
+Unblock-File -Path "..\assets\ConEmu.xml"
+copy-item "..\assets\ConEmu.xml" "$env:ProgramFiles\ConEmu\"
+
+#
+# Visual Studio
+#
+Write-Output "installing visual studio. probably wanna go make coffee now..."
+choco install visualstudio2019enterprise --yes
+choco install visualstudio2019-workload-azure --yes
+choco install visualstudio2019-workload-netweb --yes
+choco install nuget.commandline --yes
+
 
 Write-Output "Finished! Run `choco upgrade all` to get the latest software"
